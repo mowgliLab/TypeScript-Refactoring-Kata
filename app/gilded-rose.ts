6957a10babae291e9e355edc858229765eba9d24
Alain Daussin <alaindaussin@gmail.com>
2019-01-03 10:25:17 +0100

    Understand and comment process

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -15,20 +15,26 @@ export class GildedRose {
             // If sulfura, do nothing
             if (currentItem.name.indexOf(ItemTypeEnum.Sulfuras) < 0) {
                 currentItem.sellIn = currentItem.sellIn - 1;
+
+                // Normal case (decreasing case)
                 if (currentItem.name != 'Aged Brie' && currentItem.name != 'Backstage passes to a TAFKAL80ETC concert') {
                     // Don't pass under 0 quality.
                     if (currentItem.quality > 0) {
                         currentItem.quality = currentItem.quality - 1
                     }
                 } else {
+                    // Other cases (increasing)
+                    // don't go above 50
                     if (currentItem.quality < 50) {
                         currentItem.quality = currentItem.quality + 1;
                         if (currentItem.name == 'Backstage passes to a TAFKAL80ETC concert') {
+                            // If backstage and less than 10 days, increase once more (+2)
                             if (currentItem.sellIn < 11) {
                                 if (currentItem.quality < 50) {
                                     currentItem.quality = currentItem.quality + 1
                                 }
                             }
+                            // If backstage and less than 5 days, increase once more (+3)
                             if (currentItem.sellIn < 6) {
                                 if (currentItem.quality < 50) {
                                     currentItem.quality = currentItem.quality + 1
@@ -37,16 +43,21 @@ export class GildedRose {
                         }
                     }
                 }
+
+                // case when sell in is less than 0
                 if (currentItem.sellIn < 0) {
                     if (currentItem.name != 'Aged Brie') {
                         if (currentItem.name != 'Backstage passes to a TAFKAL80ETC concert') {
+                            // Normal case decrease once more (-2)
                             if (currentItem.quality > 0) {
                                 currentItem.quality = currentItem.quality - 1
                             }
                         } else {
+                            // backstage, set to zero if date is passed.
                             currentItem.quality = currentItem.quality - currentItem.quality
                         }
                     } else {
+                        // case of aged brie increase once more if less than 50 (+2)
                         if (currentItem.quality < 50) {
                             currentItem.quality = currentItem.quality + 1
                         }
